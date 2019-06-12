dfb0ae15859ec128de04d964e627f8be128ca176
neacsiuandrei <51759858+neacsiuandrei@users.noreply.github.com>
2019-06-12 21:26:18 +0300

    Add files via upload

---
+++ SDD_Stack/SDD_Stack/Source.cpp
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <string.h>
+
+struct Student {
+	int id;
+	char name[50];
+	int age;
+};
+
+struct Node {
+	Node* next;
+	Student* student;
+};
+
+struct Stack {
+	Node* head = NULL;
+};
+
+Student* initStudent(int id, const char* name, int age) {
+	Student* s = (Student*)malloc(sizeof(Student));
+	s->id = id;
+	strcpy(s->name, name);
+	s->age = age;
+	return s;
+}
+
+void printStudent(Student* s) {
+	printf("ID: %d - Name: %s - Age: %d\n", s->id, s->name, s->age);
+}
+
+Node* initNode(Student* s) {
+	Node* newNode = (Node*)malloc(sizeof(Node));
+	newNode->next = NULL;
+	newNode->student = initStudent(s->id, s->name, s->age);
+	return newNode;
+}
+
+Stack* initList() {
+	Stack* list = (Stack*)malloc(sizeof(Queue));
+	list->head = NULL;
+	return list;
+}
+
+void addNode(Stack* list, Student* s) {
+	Node* newNode = initNode(s);
+	newNode->next = list->head;
+	list->head = newNode;
+}
+
+void printList(Stack* list) {
+	if (list->head == NULL)
+		printf("No list exists.\n");
+	else {
+		Node* iterator = list->head;
+		while (iterator != NULL) {
+			printStudent(iterator->student);
+			iterator = iterator->next;
+		}
+	}
+}
+
+Student* getLast(Stack* list) {
+	if (list->head == NULL) {
+		Student* s = initStudent(0, " ", 0);
+		return s;
+	}
+	Student* s = list->head->student;
+	list->head = list->head->next;
+	return s;
+}
+
+void main() {
+	Student** students = (Student**)malloc(sizeof(Student*) * 5);
+	students[0] = initStudent(1, "Andrei", 20);
+	students[1] = initStudent(2, "Alice", 18);
+	students[2] = initStudent(3, "Adi", 22);
+	students[3] = initStudent(4, "Bob", 44);
+	students[4] = initStudent(5, "Vader", 27);
+	Stack* list = initList();
+	for (int i = 0; i < 5; i++)
+		addNode(list, students[i]);
+	printList(list);
+
+	printf("\n\nGet students\n\n");
+	printStudent(getLast(list));
+	printStudent(getLast(list));
+	printStudent(getLast(list));
+
+
+	printf("\n\nCurrent stack\n\n");
+	printList(list);
+
+}
